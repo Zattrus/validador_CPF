9f299ea245a4d5904f30a7bb13913f4a896fae0d
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2022-05-12 08:18:49 -0300

    Criando funcoes para aparecer a mensagem de 'validado' ou 'erro'

--- validador_CPF.js
+++ validador_CPF.js
@@ -1,5 +1,19 @@
 console.log("Carregou o arquivo validador_CPF.js");
 
+function validaCPF(cpf) {
+  return false;
+}
+
 function validacao() {
   console.log("Fui clicado");
+
+  var cpf = document.getElementById('cpf_digitado').value;
+
+  var resultadoValidacao = validaCPF(cpf);
+
+  if (resultadoValidacao) {
+    document.getElementById('success').style.display = 'block';
+  } else {
+    document.getElementById('error').style.display = 'block';
+  }
 }
