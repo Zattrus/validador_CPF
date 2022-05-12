c7320c96df700087e44ac15e3fa756c9e62489ff
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2022-05-12 08:54:15 -0300

    Validando CPF

--- validador_CPF.js
+++ validador_CPF.js
@@ -1,19 +1,31 @@
 console.log("Carregou o arquivo validador_CPF.js");
 
 function validaCPF(cpf) {
-  return false;
+    console.log(cpf.length)
+    if (cpf.length != 11) {
+        return false;
+    } else {
+        var numeros = cpf.substring(0, 9);
+        var digitos = cpf.substring(9);
+
+        var soma;
+        for (var i = 10; i > 1; i--) {
+            soma += numeros.charAt(10 - i) * i;
+        }
+        return true;
+    }
 }
 
 function validacao() {
-  console.log("Fui clicado");
+    console.log("Fui clicado");
 
-  var cpf = document.getElementById('cpf_digitado').value;
+    var cpf = document.getElementById('cpf_digitado').value;
 
-  var resultadoValidacao = validaCPF(cpf);
+    var resultadoValidacao = validaCPF(cpf);
 
-  if (resultadoValidacao) {
-    document.getElementById('success').style.display = 'block';
-  } else {
-    document.getElementById('error').style.display = 'block';
-  }
+    if (resultadoValidacao) {
+        document.getElementById('success').style.display = 'block';
+    } else {
+        document.getElementById('error').style.display = 'block';
+    }
 }
