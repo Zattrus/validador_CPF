a2eadd604561c5c57e0990c820b5004d5aa710a4
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2022-05-12 09:45:35 -0300

    Validando CPF

--- validador_CPF.js
+++ validador_CPF.js
@@ -1,4 +1,4 @@
-console.log("Carregou o arquivo validador_CPF.js");
+// console.log("Carregou o arquivo validador_CPF.js");
 
 function validaCPF(cpf) {
     console.log(cpf.length)
@@ -8,20 +8,38 @@ function validaCPF(cpf) {
         var numeros = cpf.substring(0, 9);
         var digitos = cpf.substring(9);
 
-        var soma;
+        var soma = 0;
         for (var i = 10; i > 1; i--) {
             soma += numeros.charAt(10 - i) * i;
         }
         var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+        // Validando primeiro digito
         if (resultado != digitos.charAt(0)) {
             return false;
         }
+
+        soma = 0;
+        numeros = cpf.substring(0, 10);
+
+        for (var k = 11; k > 1; k--) {
+            soma += numeros.charAt(11 - k) * k;
+        }
+        resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+        // Valida segundo digito
+        if (resultado != digitos.charAt(1)) {
+            return false;
+        }
+
         return true;
     }
 }
 
 function validacao() {
     console.log("Fui clicado");
+    document.getElementById('success').style.display = 'none';
+    document.getElementById('error').style.display = 'none';
 
     var cpf = document.getElementById('cpf_digitado').value;
 
